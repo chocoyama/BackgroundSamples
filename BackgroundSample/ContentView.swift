76d7296edbbd12989d492be4053dcbc9012e9c46
Takuya Yokoyama <chocoyama@yahoo-corp.jp>
2020-03-10 12:22:21 +0900

    Initial Commit

---
+++ BackgroundSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BackgroundSample
+//
+//  Created by Takuya Yokoyama on 2020/03/10.
+//  Copyright © 2020 Takuya Yokoyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
